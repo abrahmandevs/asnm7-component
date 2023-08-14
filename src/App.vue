0da7f160fcd6b8ba16215cf4899f74b2c14a1c17
arrahmna-dev <dra390@gmail.com>
2023-08-14 23:48:55 +0600

    from m-7 carousel and cheart

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 	import { reactive } from "vue"
 	import Carousel from "./components/Carousel.vue"
+	import Chart from "./components/Chart.vue"
+
 	const data = reactive({
 		id: "myCarousel",
 		images: [
@@ -31,37 +33,11 @@
 			},
 		],
 	})
-	const images = [
-		{
-			image_title: "image 1",
-			image_url: "www.image.com",
-			image: "https://images.unsplash.com/photo-1494438639946-1ebd1d20bf85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1467&q=80",
-		},
-		{
-			image_title: "image 2",
-			image_url: "www.image.com",
-			image: "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-		},
-		{
-			image_title: "image 3",
-			image_url: "www.image.com",
-			image: "https://images.unsplash.com/photo-1558882224-dda166733046?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80",
-		},
-		{
-			image_title: "image 4",
-			image_url: "www.image.com",
-			image: "https://images.unsplash.com/photo-1562664377-709f2c337eb2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-		},
-		{
-			image_title: "image 5",
-			image_url: "www.image.com",
-			image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1473&q=80",
-		},
-	]
 </script>
 
 <template>
 	<Carousel :images="data.images" :id="data.id" />
+	<Chart />
 </template>
 
 <style scoped></style>
--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,19 +1,25 @@
 <script setup>
 	import { ref, onMounted, onBeforeUnmount, nextTick } from "vue"
-	defineProps(["id", "images"])
+	const props = defineProps(["id", "images"])
 
 	let carousel = null
 
 	onMounted(() => {
-		carousel = new Flickity("#carousel", {})
+		const carouselId = document.getElementById(props.id)
+		carousel = new Flickity(carouselId, {})
 	})
 	onBeforeUnmount(() => {
 		carousel.destroy()
+		// nextTick(() => {
+		//     carousel.destroy()
+		// })
 	})
+	console.log(props.id)
 </script>
 
 <template>
-	<div class="mx-auto items" id="carousel">
+	<h2 class="">Task-1 Carousel</h2>
+	<div class="mx-auto items" :id="props.id">
 		<div :style="`background-image:url(${image.image})`" class="item bg-cover bg-[#ccc]" v-for="(image, index) in images" :key="index">
 			{{ image.image_title }}
 		</div>
